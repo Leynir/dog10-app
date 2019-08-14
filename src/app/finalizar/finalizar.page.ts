5aedf3deac09325e77bdd3bd67ae39f6787e3240
Lenilson Eduardo <leynirmage@gmail.com>
2019-08-13 22:25:47 -0300

    Atividade dog10 13/08

---
+++ src/app/finalizar/finalizar.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-finalizar',
+  templateUrl: './finalizar.page.html',
+  styleUrls: ['./finalizar.page.scss'],
+})
+export class FinalizarPage implements OnInit {
+
+  constructor(public alertController: AlertController) { }
+
+  ngOnInit() {
+  }
+
+ 
+
+ 
+
+
+}
